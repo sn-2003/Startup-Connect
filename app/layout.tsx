71cf853c545ba1dbb9bf4f901e82e7c28c755230
sn-2003 <nikhil24112003@gmail.com>
2025-07-27 18:51:44 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { AuthProvider } from '@/hooks/use-auth';
 import { SessionProvider } from 'next-auth/react';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,6 +15,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-B3WWJWVEYT"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B3WWJWVEYT');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <SessionProvider>
           <AuthProvider>
